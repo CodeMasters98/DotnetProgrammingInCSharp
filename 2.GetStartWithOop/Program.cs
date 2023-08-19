87fe5bb678afc2c8067a65ab3b370edd553f4713
darvishiparham14@gmail.com <darvishiparham14@gmail.com>
2023-08-19 17:50:35 +0330

    Add project files.

---
+++ 2.GetStartWithOop/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DotnetProgrammingInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Age = 26;
+            person.FirstName = "Parham";
+            person.LastName = "Darvishi";
+            person.PhoneNumber = "09129564205";
+        }
+    }
+
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Age { get; set; }
+    }
+}
