748f2327aaa2e9d17c8c6bdcbc630feec124944b
darvishiparham14@gmail.com <darvishiparham14@gmail.com>
2023-08-27 16:10:02 +0330

    First Init

--- 1.GetStartWithWindowsApplications/Form1.cs
+++ 1.GetStartWithWindowsApplications/Form1.cs
@@ -6,5 +6,17 @@ namespace _1.GetStartWithWindowsApplications
         {
             InitializeComponent();
         }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            string username = usernameTextBox.Text;
+            string password = passwordTextBox.Text;
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please enter Username and Password!");
+                return;
+            }
+        }
+
     }
 }
