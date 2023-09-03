8bae9d9215f53523e4dad5ee2ea8e2d9b0ff1146
darvishiparham14@gmail.com <darvishiparham14@gmail.com>
2023-09-03 16:48:45 +0330

    Change

---
+++ EnumInCSharp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//Accessing Enum
+using EnumInCSharp.Enums;
+
+Days today = Days.Monday;
+
+Permissions permissions = Permissions.Read | Permissions.Write;
+
+Console.ReadLine();
