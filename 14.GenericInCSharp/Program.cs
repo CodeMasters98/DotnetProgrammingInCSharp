e72271e83a225dfa1d6e138f9a3805f935f337fe
darvishiparham14@gmail.com <darvishiparham14@gmail.com>
2023-09-10 15:07:35 +0330

    Generics

---
+++ 14.GenericInCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] intArray = { 1, 2, 3 };
+double[] doubleArray = { 1.0, 2.0, 3.0 };
+string[] stringArray = { "1",  "2", "3" };
+
+DisplayElements(intArray);
+DisplayElements(doubleArray);
+DisplayElements(stringArray);
+
+static void DisplayElements<T>(T[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write($"{item} ");
+    }
+}
