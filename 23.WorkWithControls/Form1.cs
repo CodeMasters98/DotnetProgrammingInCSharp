c313dce71ac82d0a07d523812fa32283481978f1
darvishiparham14@gmail.com <darvishiparham14@gmail.com>
2023-09-19 12:46:17 +0330

    Work With Controls

---
+++ 23.WorkWithControls/Form1.cs
@@ -0,0 +1,30 @@
+namespace _23.WorkWithControls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addNewMemberToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addNewBookToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void showCurrentListToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void showCurrentListToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
